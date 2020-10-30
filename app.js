b6a74ec6ceeeb0a09b55682813d3b4e4066eb541
RAKSHIT SHARMA <sharmarakshit1007@gmail.com>
2020-10-30 17:26:52 +0530

    Added my project

---
+++ app.js
@@ -0,0 +1,39 @@
+//MOVEMENT ANIMATION
+const card =document.querySelector('.card');
+const container = document.querySelector('.container');
+
+const title = document.querySelector(".title");
+const sneaker = document.querySelector(".sneaker img");
+const purchase = document.querySelector(".purchase");
+const description = document.querySelector(".info h3");
+const sizes = document.querySelector(".sizes");
+//MOVING ANIMATION EVENT
+container.addEventListener("mousemove",(e) => {
+let xAxis = (window.innerWidth / 2 - e.pageX)/10;
+console.log(window.innerWidth);
+let yAxis =(window.innerHeight / 2 - e.pageY)/10;
+card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+});
+
+//ANIMATE IN
+container.addEventListener("mouseenter", e =>{
+    card.style.transition = "none";
+    //Popout
+    title.style.transform = "translateZ(150px)";
+    sneaker.style.transform = "translateZ(200px) rotateZ(-45deg)";
+    //console.log("hi");
+    description.style.transform = "translateZ(125px)";
+    sizes.style.transform = "translateZ(100px)";
+    purchase.style.transform = "translateZ(75px)";;
+});
+
+//ANIMATE OUT
+container.addEventListener("mouseleave", e =>{
+    card.style.transition= "all 0.5s ease";
+    card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+    title.style.transform = "translateZ(0px)"
+    sneaker.style.transform = "translateZ(0px) rotateZ(0deg)";
+    description.style.transform = "translateZ(0px)";
+    sizes.style.transform = "translateZ(0px)";
+    purchase.style.transform = "translateZ(0px)";;
+})
